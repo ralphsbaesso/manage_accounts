88b97d4bf704bda5428ab45469544848197b2474
ralphsbaesso <ralph.baesso@fatec.sp.gob.br>
2019-01-23 22:10:38 -0200

    implementando filtro para Transfer

---
+++ app/models/util.rb
@@ -0,0 +1,20 @@
+module Util
+  class Month
+    def self.all
+      [
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro'
+      ]
+    end
+  end
+end
